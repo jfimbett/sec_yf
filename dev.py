ef8b809b05470ef5563558a945cbf27f683b5a28
Juan Imbett <juan.imbet@gmail.com>
2024-10-28 17:57:25 +0100

    feat: dev.py file as an example

--- dev.py
+++ dev.py
@@ -1,5 +1,9 @@
 #%%
 from sec_yf.download_sec import download_and_prepare_data
+from sec_yf.yahoo_finance import get_all_data
 
-if __name__ == '__main__':
-    download_and_prepare_data()
+# Download and prepare data from SEC
+download_and_prepare_data()
+
+# Get all data from Yahoo Finance
+get_all_data()
